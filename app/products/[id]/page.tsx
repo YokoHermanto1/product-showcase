a5c8af15243b5d86c7f98a4e6dd05c93b69efd70
YokoHermanto1 <yokohermanto@gmail.com>
2025-06-28 00:17:46 +0700

    Changing Code

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,11 @@
 import { getProductById } from '@/lib/api';
 import FavoriteButton from '../../components/FavoriteButton';
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-export default async function ProductDetailPage({ params }: Props) {
+export default async function ProductDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const product = await getProductById(params.id);
 
   return (
